26e2eec05fc1d53daae15e623179f765e780adc7
PeterChisanga <peterchisangamwamba@gmail.com>
2022-10-03 12:54:27 +0100

    finished author routes

--- routes/books.js
+++ routes/books.js
@@ -1,33 +1,23 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const express = require('express')
+const router = express.Router()
 const Book = require('../models/book')
-const Author = require('../models/author');
-const uploadPath = path.join('public', Book.coverImageBasePath);
-const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif']
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) => {
-        callback(null, file)
-    }
-})
+const Author = require('../models/author')
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-//All Books Route
+// All Books Route
 router.get('/', async (req, res) => {
     let query = Book.find()
     if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
     if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-        query = query.lte('publishedDate', req.query.publishedBefore)
+        query = query.lte('publishDate', req.query.publishedBefore)
     }
     if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-        query = query.gte('publishedDate', req.query.publishedAfter)
+        query = query.gte('publishDate', req.query.publishedAfter)
     }
     try {
-        const books = await Book.find({});
+        const books = await query.exec()
         res.render('books/index', {
             books: books,
             searchOptions: req.query
@@ -35,41 +25,32 @@ router.get('/', async (req, res) => {
     } catch {
         res.redirect('/')
     }
-
 })
 
-//New Book Route
+// New Book Route
 router.get('/new', async (req, res) => {
     renderNewPage(res, new Book())
 })
 
-//Create Book Route
-router.post('/', upload.single('cover'), async (req, res) => {
-    const fileName = req.file != null ? req.file.filename : null
+// Create Book Route
+router.post('/', async (req, res) => {
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
-        publishDAte: new Date(req.body.publishDate),
-        pageCount: fileName,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
         description: req.body.description
     })
+    saveCover(book, req.body.cover)
+
     try {
         const newBook = await book.save()
         // res.redirect(`books/${newBook.id}`)
         res.redirect(`books`)
     } catch {
-        if (book.coverImageName != null) {
-            removeBookCover(book.coverImageName)
-        }
         renderNewPage(res, book, true)
     }
-});
-
-function removeBookCover(fileName) {
-    fs.unlink(path.join(uploadPath, fileName), err => {
-        if (err) console.error(err)
-    })
-}
+})
 
 async function renderNewPage(res, book, hasError = false) {
     try {
@@ -85,4 +66,13 @@ async function renderNewPage(res, book, hasError = false) {
     }
 }
 
-module.exports = router;
+function saveCover(book, coverEncoded) {
+    if (coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
+}
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,21 +5,19 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
-const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
 const bookRouter = require('./routes/books');
 
-
-
-// app.set('views', path.join(__dirname + 'views'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
@@ -31,7 +29,6 @@ const mongoose = require('mongoose');
 // });
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
-    // useUnifiedTopology: true
 })
 const db = mongoose.connection;
 db.on('error', error => console.error(error))
