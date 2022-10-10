42001193bd6eb09c4b313d06128e609ed64e9517
PeterChisanga <peterchisangamwamba@gmail.com>
2022-10-10 10:04:29 +0100

    finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,15 +1,29 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.createElement)
+
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null) {
+    ready()
+} else {
+    document.getElementById('main-css')
+        .addEventListener('load', ready)
+}
+
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+    FilePond.registerPlugin(
     | FilePondPluginImagePreview,
     | FilePondPluginImageResize,
     | FilePondPluginImageEncode,
 
-)
+    )
 
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+    FilePond.setOptions({
+        stylePanelAspectRatio: 150 / 100,
+        imageResizeTargetWidth: 100,
+        imageResizeTargetHeight: 150
+    })
 
-FilePond.parse(document.body);
+    FilePond.parse(document.body);
+}
